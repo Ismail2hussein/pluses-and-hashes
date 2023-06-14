71764b29107157e720bfbaa66bdf2c11645d2aec
Ismail2hussein <ihusseinmohamed629@gmail.com>
2023-06-14 14:01:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// Good Luck 💪🏾
+function plusHashCount(str) {
+  let hashCount = 0;
+  let plusCount = 0;
+  
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '#') {
+      hashCount++;
+    } else if (str[i] === '+') {
+      plusCount++;
+    }
+  }
+  
+  return [hashCount, plusCount];
+}
+console.log(plusHashCount("###+")); // [1, 3]
+console.log(plusHashCount("##+++#")); // [3, 3]
+console.log(plusHashCount("#+++#+#++#")); // [6, 4]
+console.log(plusHashCount("")); // [0, 0]
